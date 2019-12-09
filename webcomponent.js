d6c049ad6af42e06694944632999d07f3a2bbe3c
Anna <annak@iprosis.com>
2019-12-09 11:32:21 +0200

    2

--- webcomponent.js
+++ webcomponent.js
@@ -1,122 +1,31 @@
 (function()  {
 let tmpl = document.createElement('template');
 tmpl.innerHTML = `
-  <style>
+  <stryle>
+  body { background-color:#555888; font-family:sans-serif; color:#fff; text-align:center }
+  code { display:inline-block; max-width:600px;  padding:80px 0 0; line-height:1.5; font-family:monospace; color:#ccc }
+  
+  .sc-gauge  { width:200px; height:200px; margin:200px auto; }
+  .sc-background { position:relative; height:100px; margin-bottom:10px; background-color:#fff; border-radius:150px 150px 0 0; overflow:hidden; text-align:center; }
+  .sc-mask { position:absolute; top:20px; right:20px; left:20px; height:80px; background-color:#555888; border-radius:150px 150px 0 0 }
+  .sc-percentage { position:absolute; top:100px; left:-200%; width:400%; height:400%; margin-left:100px; background-color:#00aeef; }
+  .sc-percentage { transform:rotate(120deg); transform-origin:top center; }
+  .sc-min { float:left; }
+  .sc-max { float:right; }
+  .sc-value { position:absolute; top:50%; left:0; width:100%;  font-size:48px; font-weight:700 }
+  </style>
 
-  <div class="container">
-  <div class="gauge-base">
-    <div class="gauge">
-      <div class="mask"></div>
-      <div class="needle"></div>
-      <div class="spinner"></div>
-    </div>
-    <p class="label">Squeeze-o-meter</p>
-    <div class="number">43.00</div>
+<div class="sc-gauge">
+  <div class="sc-background">
+	<div class="sc-percentage"></div>
+	<div class="sc-mask"></div>
+	<span class="sc-value">60</span>
   </div>
+  <span class="sc-min">0</span>
+  <span class="sc-max">100</span>
 </div>
 
-.container {
-	position: relative;
-	width: 400px;
-	margin: 50px auto 0;
-	overflow: hidden;
-  }
-  
-  .gauge-base {
-	position: relative;
-	width: 400px;
-	height: 400px;
-	background: #323232;
-	border-radius: 400px;
-	overflow: hidden;
-	box-shadow: 0 1px 5px rgba(0,0,0,0.5);
-	margin: 0 auto;
-	
-	.label {
-	  color: #fff;
-	  text-transform: uppercase;
-	  text-align: center;
-	  font-family: 'Alfa Slab One';
-	  letter-spacing: 5px;
-	  text-shadow: 2px 2px 2px rgba(0,0,0,0.5);
-	  margin-bottom: 20px;
-	}
-	
-	.number {
-	  text-align: center;
-	  background: #212121;
-	  color: #fff;
-	  font-size: 25px;
-	  letter-spacing: 2px;
-	  width: 150px;
-	  margin: 0 auto;
-	  padding: 10px 0;
-	  font-family: 'Alfa Slab One';
-	}
-	
-	.label, .number {
-	  position: relative;
-	  bottom: 15px;
-	}
-  }
-  
-  .gauge {
-	position: relative;
-	width: 400px;
-	height: 260px;
-	background: linear-gradient(to right, #12bc9b, #fbd35b, #f56464);
-	border-radius: 400px 400px 0 0;
-	overflow: hidden;
-	
-	.mask {
-	  position: relative;
-	  width: 380px;
-	  height: 380px;
-	  margin: 0 auto;
-	  top: 10px;
-	  background: #323232;
-	  border-radius: 400px;
-	  overflow: hidden;
-	}
-	
-	.needle, .spinner {
-	  position: absolute;
-	  bottom: 50px;
-	  left: 50%;
-	}
-	  
-	.needle {
-	  height: 0;
-	  width: 0;
-	  bottom: 70px;
-	  margin-left: -10px;
-	  border-left: 10px solid transparent;
-	  border-right: 10px solid transparent;
-	  border-bottom: 200px solid #696969;
-	  -webkit-transform-origin: 50% 100%;
-	  transform-origin: 50% 100%;
-	  @include animation(oscillate ease-in-out 1s alternate infinite);
-	}
-  
-	.spinner {
-	  height: 40px;
-	  width: 40px;
-	  margin-left: -20px;
-	  border-radius: 20px;
-	  background: #696969;
-  
-	  &::after {
-		position: absolute;
-		content: "";
-		top: 15px;
-		left: 15px;
-		height: 10px;
-		width: 10px;
-		border-radius: 10px;
-		background: #323232;
-	  }
-	}
-  }
+
 `;
 
 class Gauge extends HTMLElement {
