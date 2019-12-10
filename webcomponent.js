b34d823bff2368df360ed6dc5f577e5e5f5398ce
Anna <annak@iprosis.com>
2019-12-10 10:48:28 +0200

    1

--- webcomponent.js
+++ webcomponent.js
@@ -30,35 +30,17 @@ class Gauge extends HTMLElement {
 		this._shadowRoot = this.attachShadow({mode: 'open'});
 		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 		this.style.height = "100%";
-		//this._shadowRoot.addEventListener("mousedown", this.makeMouseDownHandler(this.mouseStart.bind(this), this.mouseFeedback.bind(this), this.mouseFinish.bind(this), this.mouseAbort.bind(this)));
-		this._val = 0;
-		this._look = "vu";
-		this._rotate_angle = 360; // depends on used picture
-		this.needle = this._shadowRoot.querySelector("#needle");
 
 	}; // end of constructor
 
-	
-    get val() {
-    	return Math.round(this._val);
-    }
-    set val(value) {
-		this._val =  Math.max(0, Math.min(100, value));
-		var angle = this._val / 100 * this._rotate_angle;
-		//this.needle.style.transform = "rotate(" + angle + "deg)";
-	};
-
-
-	get look() {
-		return this._look;
+	/* getter of value */
+	set setValuel(newValue) {
+		this._shadowRoot.getElementById("sc-value").value = newValue;
 	}
-
-	set look(value) {
-		this._look = value;
-		this.val = this.val; // Refresh to fit to new scaling
-	};
-
-
+	/* setter of value*/
+	get getValue() {
+		return this._shadowRoot.getElementById("sc-value").value;
+	}
 
   }
   customElements.define('com-iprosis-sample-gauge', Gauge);
