0d0ed7954274367ebe3267a85ab188866f97768a
Anna <annak@iprosis.com>
2019-12-10 09:40:02 +0200

    1

--- webcomponent.js
+++ webcomponent.js
@@ -1,6 +1,8 @@
 (function()  {
 let tmpl = document.createElement('template');
 tmpl.innerHTML = `
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+       <div id="chart_div" style="width: 400px; height: 120px;"></div>
 
 `;
 
@@ -17,10 +19,38 @@ class Gauge extends HTMLElement {
 		this._rotate_angle = 360; // depends on used picture
 		this.needle = this._shadowRoot.querySelector("#needle");
 
+		google.charts.load('current', {'packages':['gauge']});
+	google.charts.setOnLoadCallback(drawChart);
+
+	function drawChart() {
+
+	  var data = google.visualization.arrayToDataTable([
+		['Label', 'Value'],
+		['SOMETHING', 80]
+
+	  ]);
+
+	  var options = {
+		width: 400, height: 120,
+		redFrom: 90, redTo: 100,
+		yellowFrom:75, yellowTo: 90,
+		greenFrom:0,greenTo:75,
+		minorTicks: 5
+	  };
+
+	  var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+	  chart.draw(data, options);
+	  setInterval(function() {
+		data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+		chart.draw(data, options);
+	  }, 26000);
+	}
+
+
 	};
 
-
-
+	
     get val() {
     	return Math.round(this._val);
     }
