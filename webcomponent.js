b7cc415a27dc80b2333f871ed7757ccf07b07674
Anna <annak@iprosis.com>
2019-12-23 15:53:02 +0200

    123

--- webcomponent.js
+++ webcomponent.js
@@ -176,7 +176,7 @@ class Gauge extends HTMLElement {
 			//console.log("this.value " + this.value);
 			//var angle = this._val / 100 * this._rotate_angle;
 			//console.log("angle "+angle);
-			this.value.innerHTML(newValue + "%");
+			this.value.innerHTML.content = (newValue + "%");
 				this._deg = (newValue * 177.5) / 100;
 	
 				//gauge_value.html(value + "%");
