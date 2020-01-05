a9fe8d493e1bc7ed7cedf9451083b38b09b918de
Anna <annak@iprosis.com>
2020-01-05 17:36:47 +0200

    5

--- webcomponent.js
+++ webcomponent.js
@@ -9,6 +9,7 @@ class Gauge extends HTMLElement {
 
 	constructor() {
 		super();
+		var echarts = require('echarts');
 		this._shadowRoot = this.attachShadow({mode: 'open'});
 		this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
 		var dataResultSet = null;
